8618aca989faa0b617e3775de288c0eb9a43bf50
Hugo Bastien <hugobast@gmail.com>
2013-02-28 14:08:35 -0500

    Removed amazon and google sensitive information
    Conflicts:
    	spec/fixtures/fetch_amazon.yml
    	spec/fixtures/fetch_google.yml
    	spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,26 @@
 require 'emporium'
 require 'yaml'
 
+<<<<<<< HEAD
 CONFIG = YAML.load_file('./spec/config.yml')
 AMAZON = CONFIG["amazon"]
 GOOGLE = CONFIG["google"]
+=======
+CONFIG = YAML.load_file('spec/config.yml')
+AMAZON = CONFIG["amazon"]
+GOOGLE = CONFIG["google"]
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<access_key>') { AMAZON["access_key"] }
+  config.filter_sensitive_data('<associate_tag>') { AMAZON["associate_tag"] }
+  config.filter_sensitive_data('<secret>') { AMAZON["secret"] }
+  config.filter_sensitive_data('<access_key>') { GOOGLE["access_key"] }
+  config.filter_sensitive_data('<cse>') { GOOGLE["cse"] }
+  config.default_cassette_options = {
+    :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:Timestamp, :Signature, :AssociateTag, :key)]
+  }
+end
+
+>>>>>>> 5c13f55... Removed amazon and google sensitive information
